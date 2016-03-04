bf981fa3c960e34a5080dc2a0f251a71975ac353
MrGenga <mrgenga537@gmail.com>
2016-03-04 05:23:20 +0300

    Debug on

--- src/main/java/io/github/mrgenga/bouncyball/network/PacketIntercepter.java
+++ src/main/java/io/github/mrgenga/bouncyball/network/PacketIntercepter.java
@@ -11,6 +11,7 @@ import static io.github.mrgenga.bouncyball.network.PacketIDs.*;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.util.Util;
 
 import io.github.jython234.jraklibplus.protocol.raknet.*;
 import io.github.jython234.jraklibplus.protocol.raknet.CustomPackets.*;
@@ -31,7 +32,7 @@ public class PacketIntercepter {
             ByteBuffer bb = ByteBuffer.wrap(buffer);
             byte pid = bb.get();
 
-            //server.getLogger().debug("Intercepting packet "+ Util.toHex(pid)+" (ToServer: "+Boolean.toString(toServer)+")");
+            server.getLogger().debug("Intercepting packet "+ Util.toHex(pid)+" (ToServer: "+Boolean.toString(toServer)+")");
 
             if(pid <= RAKNET_CUSTOM_PACKET_MAX && pid >= RAKNET_CUSTOM_PACKET_MIN){ //Custom Packet
                 switch(pid){
@@ -102,59 +103,56 @@ public class PacketIntercepter {
         } else {
                 pid = ep.payload[1]; //second byte
         }
-        //server.getLogger().debug("Intercepting packet "+ Util.toHex(pid)+" (ToServer: "+Boolean.toString(toServer)+")");
+        server.getLogger().debug("Intercepting packet "+ Util.toHex(pid)+" CustomPacket PID: "+Util.toHex(cPID)+"(ToServer: "+Boolean.toString(toServer)+")");
 
-        if(cPID == (byte)0x84){
-
-            switch(pid){
-                case LOGIN_PACKET:
-                    LoginPacket lp = new LoginPacket();
-                    lp.decode(ep.payload);
-                    if(!lp.isCorrect){
-                        return;
-                    }
-                    server.getLogger().info(lp.username+"["+session.getAddress().toString()+"] logged into the proxy. (Protocol "+lp.protocol+")");
-
-                    session.setUsername(lp.username);
+        switch(pid){
+            case LOGIN_PACKET:
+                LoginPacket lp = new LoginPacket();
+                lp.decode(ep.payload);
+                if(!lp.isCorrect){
                     return;
-
-                case MOVE_PLAYER_PACKET:
-                    if(!session.hasSpawned()){
-                        session.setSpawned(true);
-                        server.getLogger().debug("Spawned!");
-                    }
-                    return;
-
-              /*case DISCONNECT_PACKET:
-                    DisconnectPacket dp = new DisconnectPacket();
-                    dp.decode(ep.payload);
-
-                    if(!dp.isCorrect){
-                        return;
-                    }
-
-                    session.getRemoteServer().setRunning(false);
-
-                    server.serverSessions.remove(session.getRemoteServer().getAddress().toString());
-                    server.clientSessions.remove(session.getAddress().toString());
-
-                    if(toServer) {
-                        server.getLogger().info(session.getUsername() + "[" + session.getAddress().toString() + "] disconnected: disconnected by client: "+dp.message);
-                    } else {
-                        server.getLogger().info(session.getUsername() + "[" + session.getAddress().toString() + "] disconnected: disconnected by server: "+dp.message);
-                    }
-                    return;*/
-
-            /*case MC_MESSAGE_PACKET:
-                if(toServer) { //To prevent private messages from being displayed.
-                    MessagePacket mp = new MessagePacket();
-                    mp.decode(ep.buffer);
-                    if(server.logChat()) {
-                        server.getLogger().info("[Server: " + session.getRemoteServer().getAddress().toString() + "] " + mp.message);
-                    }
                 }
-                break;*/
+                server.getLogger().info(lp.username+"["+session.getAddress().toString()+"] logged into the proxy. (Protocol "+lp.protocol+")");
+
+                session.setUsername(lp.username);
+                return;
+
+            case MOVE_PLAYER_PACKET:
+                if(!session.hasSpawned()){
+                    session.setSpawned(true);
+                    server.getLogger().debug("Spawned!");
+                }
+                return;
+
+          /*case DISCONNECT_PACKET:
+                DisconnectPacket dp = new DisconnectPacket();
+                dp.decode(ep.payload);
+
+                if(!dp.isCorrect){
+                    return;
+                }
+
+                session.getRemoteServer().setRunning(false);
+
+                server.serverSessions.remove(session.getRemoteServer().getAddress().toString());
+                server.clientSessions.remove(session.getAddress().toString());
+
+                if(toServer) {
+                    server.getLogger().info(session.getUsername() + "[" + session.getAddress().toString() + "] disconnected: disconnected by client: "+dp.message);
+                } else {
+                    server.getLogger().info(session.getUsername() + "[" + session.getAddress().toString() + "] disconnected: disconnected by server: "+dp.message);
+                }
+                return;*/
+
+        /*case MC_MESSAGE_PACKET:
+            if(toServer) { //To prevent private messages from being displayed.
+                MessagePacket mp = new MessagePacket();
+                mp.decode(ep.buffer);
+                if(server.logChat()) {
+                    server.getLogger().info("[Server: " + session.getRemoteServer().getAddress().toString() + "] " + mp.message);
+                }
             }
+            break;*/
         }
         switch(pid){
             case MC_DISCONNECT_NOTIFICATION:
