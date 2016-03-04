5f2e175cdee5e07604b74530bffb1829e6f91fcd
MrGenga <mrgenga537@gmail.com>
2016-03-04 05:37:14 +0300

    Fix DisconnectPacket and LoginPacket

--- src/main/java/io/github/mrgenga/bouncyball/network/PacketIntercepter.java
+++ src/main/java/io/github/mrgenga/bouncyball/network/PacketIntercepter.java
@@ -31,7 +31,7 @@ public class PacketIntercepter {
             ByteBuffer bb = ByteBuffer.wrap(buffer);
             byte pid = bb.get();
 
-            server.getLogger().debug("Intercepting packet "+ Util.toHex(pid)+" (ToServer: "+Boolean.toString(toServer)+")");
+            //server.getLogger().debug("Intercepting packet "+ Util.toHex(pid)+" (ToServer: "+Boolean.toString(toServer)+")");
 
             if(pid <= RAKNET_CUSTOM_PACKET_MAX && pid >= RAKNET_CUSTOM_PACKET_MIN){ //Custom Packet
                 switch(pid){
@@ -104,55 +104,57 @@ public class PacketIntercepter {
         }
         //server.getLogger().debug("Intercepting packet "+ Util.toHex(pid)+" CustomPacket PID: "+Util.toHex(cPID)+"(ToServer: "+Boolean.toString(toServer)+")");
 
-        switch(pid){
-            case LOGIN_PACKET:
-                LoginPacket lp = new LoginPacket();
-                lp.decode(ep.payload);
-                if(!lp.isCorrect){
+        if(cPID == (byte)0x8c){
+            switch(pid){
+                case LOGIN_PACKET:
+                    LoginPacket lp = new LoginPacket();
+                    lp.decode(ep.payload);
+                    if(!lp.isCorrect){
+                        return;
+                    }
+                    server.getLogger().info(lp.username+"["+session.getAddress().toString()+"] logged into the proxy. (Protocol "+lp.protocol+")");
+                    //server.getLogger().debug("Intercepting packet "+ Util.toHex(pid)+" CustomPacket PID: "+Util.toHex(cPID)+"(ToServer: "+Boolean.toString(toServer)+")");
+
+                    session.setUsername(lp.username);
                     return;
-                }
-                server.getLogger().info(lp.username+"["+session.getAddress().toString()+"] logged into the proxy. (Protocol "+lp.protocol+")");
-                server.getLogger().debug("Intercepting packet "+ Util.toHex(pid)+" CustomPacket PID: "+Util.toHex(cPID)+"(ToServer: "+Boolean.toString(toServer)+")");
 
-                session.setUsername(lp.username);
-                return;
-
-            case MOVE_PLAYER_PACKET:
-                if(!session.hasSpawned()){
-                    session.setSpawned(true);
-                    server.getLogger().debug("Spawned!");
-                }
-                return;
-
-          /*case DISCONNECT_PACKET:
-                DisconnectPacket dp = new DisconnectPacket();
-                dp.decode(ep.payload);
-
-                if(!dp.isCorrect){
+                case MOVE_PLAYER_PACKET:
+                    if(!session.hasSpawned()){
+                        session.setSpawned(true);
+                        server.getLogger().debug("Spawned!");
+                    }
                     return;
+
+                case DISCONNECT_PACKET:
+                    DisconnectPacket dp = new DisconnectPacket();
+                    dp.decode(ep.payload);
+
+                    if(!dp.isCorrect){
+                        return;
+                    }
+
+                    session.getRemoteServer().setRunning(false);
+
+                    server.serverSessions.remove(session.getRemoteServer().getAddress().toString());
+                    server.clientSessions.remove(session.getAddress().toString());
+
+                    if(toServer) {
+                        server.getLogger().info(session.getUsername() + "[" + session.getAddress().toString() + "] disconnected: disconnected by client: "+dp.message);
+                    } else {
+                      server.getLogger().info(session.getUsername() + "[" + session.getAddress().toString() + "] disconnected: disconnected by server: "+dp.message);
+                    }
+                    return;
+
+            /*case MC_MESSAGE_PACKET:
+                if(toServer) { //To prevent private messages from being displayed.
+                    MessagePacket mp = new MessagePacket();
+                    mp.decode(ep.buffer);
+                    if(server.logChat()) {
+                        server.getLogger().info("[Server: " + session.getRemoteServer().getAddress().toString() + "] " + mp.message);
+                    }
                 }
-
-                session.getRemoteServer().setRunning(false);
-
-                server.serverSessions.remove(session.getRemoteServer().getAddress().toString());
-                server.clientSessions.remove(session.getAddress().toString());
-
-                if(toServer) {
-                    server.getLogger().info(session.getUsername() + "[" + session.getAddress().toString() + "] disconnected: disconnected by client: "+dp.message);
-                } else {
-                    server.getLogger().info(session.getUsername() + "[" + session.getAddress().toString() + "] disconnected: disconnected by server: "+dp.message);
-                }
-                return;*/
-
-        /*case MC_MESSAGE_PACKET:
-            if(toServer) { //To prevent private messages from being displayed.
-                MessagePacket mp = new MessagePacket();
-                mp.decode(ep.buffer);
-                if(server.logChat()) {
-                    server.getLogger().info("[Server: " + session.getRemoteServer().getAddress().toString() + "] " + mp.message);
-                }
+                break;*/
             }
-            break;*/
         }
         switch(pid){
             case MC_DISCONNECT_NOTIFICATION:
